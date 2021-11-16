24928cb888c40f8885a259207592f25fb13ffd9c
MefoBeso <sunthanhan1308@gmail.com>
2021-11-17 00:23:48 +0700

    css landing page

--- pet-shop/src/components/LandingPage.js
+++ pet-shop/src/components/LandingPage.js
@@ -6,28 +6,26 @@ function LandingPage() {
   return (
     <div className="bg bg-ln">
       <Header />
-      <div className="bg-landing">
-        <div className="container landing">
-          <span className="landing-slogan">Best things for your boss</span>
-          <span></span>
-          <span></span>
-          <a href="/home" className="landing-btn">
-            Let's explore{" "}
-          </a>
-          <span></span>
-          <span className="landing-slogan3">
-            Product from over 50 brands all over the world
+      <div className="landing">
+        <span className="landing-slogan">Best things for your boss</span>
+        <span></span>
+        <span></span>
+        <a href="/home" className="landing-btn">
+          Let's explore !{" "}
+        </a>
+        <span></span>
+        <span className="landing-slogan3">
+          Product from over 50 brands all over the world
+        </span>
+        <div className="landing-slogan2">
+          <img
+            src={process.env.PUBLIC_URL + "/slogan.jpg"}
+            alt="slogan"
+            className="landing-slogan2-img"
+          ></img>
+          <span className="landing-slogan2-text">
+            "One of the best pet shop in the world"
           </span>
-          <div className="landing-slogan2">
-            <img
-              src={process.env.PUBLIC_URL + "/slogan.jpg"}
-              alt="slogan"
-              className="landing-slogan2-img"
-            ></img>
-            <span className="landing-slogan2-text">
-              "One of the best pet shop in the world"
-            </span>
-          </div>
         </div>
       </div>
       <Footer />
