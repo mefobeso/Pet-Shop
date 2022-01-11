772ecd28e0dcf4abd054ab10217e39b9403b55bc
MefoBeso <sunthanhan1308@gmail.com>
2022-01-12 01:38:29 +0700

    Update CartQuantity.js

--- pet-shop/src/components/cart/CartQuantity.js
+++ pet-shop/src/components/cart/CartQuantity.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 
 export default function CartQuantity(props) {
   const [selectedQuantity, setSelectedQuantity] = useState(1);
@@ -14,7 +13,6 @@ export default function CartQuantity(props) {
       setSelectedQuantity(() => {
         setSelectedQuantity(selectedQuantity);
       });
-      console.log(selectedQuantity);
     }
     return;
   };
